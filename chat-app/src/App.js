58c8aa324355f46534bba1ce466e38dcb3eec5b4
Rogelio Aguilar <raguilar27@github.com>
2021-07-18 02:57:01 -0700

    Created login container

--- chat-app/src/App.js
+++ chat-app/src/App.js
@@ -1,17 +1,24 @@
 import { ChatEngine } from "react-chat-engine";
 
 import ChatFeed from "./components/ChatFeed";
+import LoginForm from "./components/LoginForm";
 
 import "./App.css";
+import { ProjectFilled } from "@ant-design/icons";
+
+const projectID = "d08f76a4-4f5b-473b-a032-c1ce1cacfd0a";
 
 const App = () => {
+  if (!localStorage.getItem("username")) return <LoginForm />;
+
   return (
     <ChatEngine
       height="100vh"
-      projectID="d08f76a4-4f5b-473b-a032-c1ce1cacfd0a"
-      userName="Bobby"
-      userSecret="Secret123"
+      projectID={projectID},
+      userName={localStorage.getItem("username")}
+      userSecret={localStorage.getItem("password")}
       renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+      onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
     />
   );
 };
---
+++ chat-app/src/components/LoginForm.jsx
@@ -0,0 +1,68 @@
+import { useState } from "react";
+import axios from "axios";
+
+const projectID = "d08f76a4-4f5b-473b-a032-c1ce1cacfd0a";
+
+const LoginForm = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const authObject = {
+      "Project-ID": projectID,
+      "User-Name": username,
+      "User-Secret": password,
+    };
+
+    try {
+      await axios.get("https://api.chatengine.io/chats", {
+        headers: authObject,
+      });
+
+      localStorage.setItem("username", username);
+      localStorage.setItem("password", password);
+
+      window.location.reload();
+      setError("");
+    } catch (error) {
+      setError("Incorrect Username or Password!");
+    }
+  };
+
+  return (
+    <div className="wrapper">
+      <div className="form">
+        <h1 className="title">Chat Application</h1>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            className="input"
+            placeholder="Username"
+            required
+          />
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            className="input"
+            placeholder="Password"
+            required
+          />
+          <div align="center">
+            <button type="submit" className="button">
+              <span>Start Chatting</span>
+            </button>
+          </div>
+          <h2 className="error">{error}</h2>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default LoginForm;
