b9e55575c5d7363294e5209a67f9ad099b249143
Rogelio Aguilar <raguilar27@github.com>
2021-07-17 20:23:13 -0700

    added chat app css and simple props for testing

--- chat-app/src/App.js
+++ chat-app/src/App.js
@@ -3,7 +3,14 @@ import { ChatEngine } from "react-chat-engine";
 import "./App.css";
 
 const App = () => {
-  return <ChatEngine height="100vh" projectID="" userName="" userSecret="" />;
+  return (
+    <ChatEngine
+      height="100vh"
+      projectID="8a1f753d-d251-4718-8cbc-efd196dfcb9c"
+      userName="BobbyFromXmen"
+      userSecret="Secret123"
+    />
+  );
 };
 
-export default app;
+export default App;
