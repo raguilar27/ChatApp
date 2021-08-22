d900ae8af3f3017bca57396e1efceb5d168bcbf3
Rogelio Aguilar <raguilar27@github.com>
2021-08-21 20:16:06 -0700

    added chats for users

--- chat-app/src/components/Chats.js
+++ chat-app/src/components/Chats.js
@@ -1,7 +1,88 @@
-import React from "react";
+import React, { useRef, useState, useEffect } from "react";
+import { useHistory } from "react-router-dom";
+import { ChatEngine } from "react-chat-engine";
+import { auth } from "./Firebase";
+
+import { useAuth } from "../contexts/AuthContext";
+import axios from "axios";
 
 const Chats = () => {
-  return <h1>Chats;</h1>;
+  const history = useHistory();
+  const { user } = useAuth();
+  const { loading, setLoading } = useState(true);
+
+  console.log(user);
+
+  const handleLogout = async () => {
+    await auth.signOut();
+    history.push("/");
+  };
+
+  const getFile = async (url) => {
+    const response = await fetch(url);
+    const data = await response.blob();
+
+    return new File([data], "userPhoto.jpg", { type: "image/jpeg" });
+  };
+
+  useEffect(() => {
+    if (!user) {
+      history.push("/");
+      return;
+    }
+
+    axios
+      .get("https://api.chatengine.io/users/me", {
+        headers: {
+          "project-id": "d08f76a4-4f5b-473b-a032-c1ce1cacfd0a",
+          "user-name": user.email,
+          "user-secret": user.uid,
+        },
+      })
+      .then(() => {
+        setLoading(false);
+      })
+      .catch(() => {
+        let formdata = new FormData();
+        formdata.append("email", user.email);
+        formdata.append("username", user.email);
+        formdata.append("secret", user.uid);
+
+        getFile(user.photoURL).then((avatar) => {
+          formdata.append("avatar", avatar, avatar.name);
+
+          axios
+            .post("https://api.chatengine.io/users", formdata, {
+              headers: {
+                "private-key": "267db406-02a6-4035-b232-be3def69feff",
+              },
+            })
+            .then(() => {
+              setLoading(false);
+            })
+            .catch((error) => console.log(error));
+        });
+      });
+  }, [user, history]);
+
+  if (!user || loading) return "Loading...";
+
+  return (
+    <div className="chats-page">
+      <div className="nav-bar">
+        <div className="logo-tab">Chat App</div>
+        <div className="logout-tab" onClick={handleLogout}>
+          Logout
+        </div>
+      </div>
+      <ChatEngine
+        height="calc(100vh - 66px)"
+        projectID="d08f76a4-4f5b-473b-a032-c1ce1cacfd0a"
+        userName="{user.email}"
+        userSecret="{user.uid}"
+      />
+    </div>
+  );
 };
 
 export default Chats;
